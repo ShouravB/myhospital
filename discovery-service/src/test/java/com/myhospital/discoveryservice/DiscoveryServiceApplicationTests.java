a9bfe903177273c90218c149c2eca264946e44df
ShouravB <shourav.bhattarai@gmail.com>
2023-10-31 21:43:48 -0700

    completed appointment service discovery

---
+++ discovery-service/src/test/java/com/myhospital/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhospital.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
